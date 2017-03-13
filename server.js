e0dc00427feab4b251a82e7b46c5f9e64a2db848
sanikak96 <kulkarnisanika99@gmail.com>
2017-03-13 21:12:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article1', function (req, res){
-    res.send('Article one requested and will be served here');
+    res.sendFile(path.join(_dirname, 'ui', 'article1.html'));
 });
 
 app.get('/article2', function (req, res){
-res.send('Article two requested and will be served here');   
+     res.sendFile(path.join(_dirname, 'ui', 'article2.html')); 
 });
     
 app.get('/article3', function (req, res){
-res.send('Article three requested and will be served here');
+     res.sendFile(path.join(_dirname, 'ui', 'article3.html'));
 });
 
 
