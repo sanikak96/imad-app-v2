e72d60cd044b68c950be39603f3b4a8aaea0b16e
sanikak96 <kulkarnisanika99@gmail.com>
2017-03-14 09:38:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'sanika.jpg'));
 });
 
 
